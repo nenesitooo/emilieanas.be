709edc427b37cc7254de18242287b58902f62a27
Anas Alaoui <anasse.alaoui@gmail.com>
2017-08-03 21:14:15 +0100

    better scroll navigation

--- src/js/components/navigation.js
+++ src/js/components/navigation.js
@@ -1,33 +1,68 @@
-// Credits to @bdc
+// Credits to @bdc - https://github.com/bendc/anchor-scroll
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
   'use strict'
 
-  var links = document.querySelectorAll('.js--scroll')
-  var i = links.length
-  var root = /firefox|trident/i.test(navigator.userAgent) ? document.documentElement : document.body
-  var easeInOutCubic = function (t, b, c, d) {
-    if ((t /= d / 2) < 1) return c / 2 * t * t * t + b
-    return c / 2 * ((t -= 2) * t * t + 2) + b
+  const root = (() => {
+    if ('scrollingElement' in document) return document.scrollingElement
+    const html = document.documentElement
+    const start = html.scrollTop
+    html.scrollTop = start + 1
+    const end = html.scrollTop
+    html.scrollTop = start
+    return end > start ? html : document.body
+  })()
+
+  const ease = (duration, elapsed, start, end) =>
+    Math.round(end * (-Math.pow(2, -10 * elapsed / duration) + 1) + start)
+
+  const getCoordinates = hash => {
+    const start = root.scrollTop
+    const delta = (() => {
+      if (hash.length < 2) return -start
+      const target = document.querySelector(hash)
+      if (!target) return
+      const top = target.getBoundingClientRect().top - 100
+      const max = root.scrollHeight - window.innerHeight
+      return start + top < max ? top : max - start
+    })()
+    if (delta) return new Map([['start', start], ['delta', delta]])
   }
 
-  while (i--) {
-    links.item(i).addEventListener('click', function (e) {
-      var startTime
-      var startPos = root.scrollTop
-      var endPos = document.getElementById(/[^#]+$/.exec(this.href)[0]).getBoundingClientRect().top - 100
-      var maxScroll = root.scrollHeight - window.innerHeight
-      var scrollEndValue = startPos + endPos < maxScroll ? endPos : maxScroll - startPos
-      var duration = 900
-      var scroll = function (timestamp) {
-        startTime = startTime || timestamp
-        var elapsed = timestamp - startTime
-        var progress = easeInOutCubic(elapsed, startPos, scrollEndValue, duration)
-        root.scrollTop = progress
-        elapsed < duration && requestAnimationFrame(scroll)
-      }
-      requestAnimationFrame(scroll)
-      e.preventDefault()
-    })
+  const scroll = link => {
+    const hash = link.getAttribute('href')
+    const coordinates = getCoordinates(hash)
+    if (!coordinates) return
+
+    const tick = timestamp => {
+      progress.set('elapsed', timestamp - start)
+      root.scrollTop = ease(...progress.values(), ...coordinates.values())
+      progress.get('elapsed') < progress.get('duration')
+      ? requestAnimationFrame(tick)
+      : complete(hash, coordinates)
+    }
+
+    const progress = new Map([['duration', 800]])
+    const start = performance.now()
+    requestAnimationFrame(tick)
   }
+
+  const complete = (hash, coordinates) => {
+    history.pushState(null, null, hash)
+    root.scrollTop = coordinates.get('start') + coordinates.get('delta')
+  }
+
+  const attachHandler = (links, index) => {
+    const link = links.item(index)
+    link.addEventListener('click', event => {
+      event.preventDefault()
+      scroll(link)
+    })
+    if (index) return attachHandler(links, index - 1)
+  }
+
+  const links = document.querySelectorAll('.js--scroll')
+  const last = links.length - 1
+  if (last < 0) return
+  attachHandler(links, last)
 })
